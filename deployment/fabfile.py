cf6314df5705a6d7845a6fe8acc5fe389ad646eb
Tom Lazar <tomster@pyfidelity.com>
2014-11-25 01:11:14 +0100

    move backend into subfolder

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -12,12 +12,12 @@ def update_app(clean=False, build=True, config_path='production.ini', **kwargs):
     Build the backend, upload it to the remote server, install it there and restart it
     """
     with fab.lcd('../backend'):
-        fab.put(config_path, '/home/halfnarp/production.ini')
+        fab.put(config_path, '/home/halfnarp/backend/production.ini')
         if value_asbool(build):
             fab.local('make sdist')
         fab.put('dist/*.tar.gz', '/tmp/backend.tgz')
-        fab.put('requirements.txt', '/home/halfnarp/requirements.txt')
-    with fab.cd('/home/halfnarp/'):
+        fab.put('requirements.txt', '/home/halfnarp/backend/requirements.txt')
+    with fab.cd('/home/halfnarp/backend/'):
         fab.sudo('bin/pip install --upgrade --allow-external argparse -r requirements.txt', user='halfnarp')
         fab.sudo('bin/pip install --upgrade --force-reinstall --no-deps /tmp/backend.tgz', user='halfnarp')
 
